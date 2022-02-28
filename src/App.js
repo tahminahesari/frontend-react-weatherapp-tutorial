4b34b8fe52d18cf1456e119777fcc7e4f5bca4ca
tahminahesari <hesari@live.com>
2022-02-28 17:52:18 +0100

    callback props, query, setlocation functie

--- src/App.js
+++ src/App.js
@@ -1,43 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import SearchBar from "./components/searchBar/SearchBar";
 import TabBarMenu from "./components/tabBarMenu/TabBarMenu";
 import MetricSlider from "./components/metricSlider/MetricSlider";
 import "./App.css";
+import axios from "axios";
+
+const apiKey = "62780ad2de9b8538cfdd84ddaafdb93a";
 
 function App() {
-  return (
-    <>
-      const apiKey = '62780ad2de9b8538cfdd84ddaafdb93a '; function App(){" "}
-      {
-        // …
-      }
-      <div className="weather-container">
-        {/*HEADER -------------------- */}
-        <div className="weather-header">
-          <SearchBar />
+  const [weatherData, setWeatherData] = useState({});
+  const [location, setLocation] = useState("");
 
-          <span className="location-details">
-            <h2>Bewolkt</h2>
-            <h3> </h3>
-            <h1>14 &deg;</h1>
+  async function fetchData() {
+    try {
+      const result = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=utrecht,nl&appid=${apiKey}&lang=nl`
+      );
+      console.log(result.data);
+      setWeatherData(result.data);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+  return <SearchBar setLocationHandler={setLocation} />;
+  <>
+    <div className="weather-container">
+      {/*HEADER -------------------- */}
+      <div className="weather-header">
+        <SearchBar />
 
-            <button type="button">Haal data op!</button>
-          </span>
-        </div>
-
-        {/*CONTENT ------------------ */}
-        <div className="weather-content">
-          <TabBarMenu />
-
-          <div className="tab-wrapper">
-            Alle inhoud van de tabbladen komt hier!
-          </div>
-        </div>
-
-        <MetricSlider />
+        <span className="location-details">
+          {Object.keys(weatherData).length > 0 && (
+            <>
+              <h2>{weatherData.weather[0].description}</h2>
+              <h3>{weatherData.name} </h3>
+              <h1>{weatherData.main.temp}</h1>
+            </>
+          )}
+          <button type="button" onClick={fetchData}>
+            Haal data op!
+          </button>
+        </span>
       </div>
-    </>
-  );
+
+      {/*CONTENT ------------------ */}
+      <div className="weather-content">
+        <TabBarMenu />
+
+        <div className="tab-wrapper">
+          Alle inhoud van de tabbladen komt hier!
+        </div>
+      </div>
+
+      <MetricSlider />
+    </div>
+  </>;
 }
 
 export default App;
--- src/components/searchBar/SearchBar.js
+++ src/components/searchBar/SearchBar.js
@@ -1,18 +1,27 @@
-import React from 'react';
-import './SearchBar.css';
+import React, { useState } from "react";
+import "./SearchBar.css";
 
-function SearchBar() {
+function SearchBar({ setLocationHandler }) {
+  const [query, setQuery] = useState("");
+
+  // wat zorgt er (e) hier voor? 6.5 callback props
+  function OnFormSubmit(e) {
+    e.preventDefault();
+    setLocationHandler(query);
+    console.log("Submitted!");
+  }
+
   return (
     <form className="searchbar">
       <input
         type="text"
         name="search"
+        value={query}
+        // waar halt hij e.target.value ?
+        onChange={(e) => setQuery(e.target.value)}
         placeholder="Zoek een stad in Nederland"
       />
-
-      <button type="submit">
-        Zoek
-      </button>
+      <button type="submit">Zoek</button>
     </form>
   );
 }
